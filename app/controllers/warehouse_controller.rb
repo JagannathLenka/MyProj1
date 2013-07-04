45476eac022a96da206d78cfcc5c094bcf9e2009
unknown <Jagannathp_lenka@BLRKEC97427L.ad.infosys.com>
2013-07-04 07:57:12 -0700

    database update

--- app/controllers/warehouse_controller.rb
+++ app/controllers/warehouse_controller.rb
@@ -14,7 +14,12 @@ def index
   bayhash= bayhash.merge({:aisleseparation => aislevalue.properties1})
     
   bay.each do |bayvalue|
-      bayhash= bayhash.merge({bayvalue.bay_id =>{:type => bayvalue.properties1 , :item => bayvalue.properties2}})
+      if bayvalue.customer_bay_id.blank?
+        customer_bay_id = bayvalue.bay_id
+      else
+        customer_bay_id = bayvalue.customer_bay_id
+      end
+      bayhash= bayhash.merge({bayvalue.bay_id =>{:type => bayvalue.properties1 , :item => bayvalue.properties2, :customerid => customer_bay_id}})
       
     end
       
@@ -23,21 +28,40 @@ def index
   end
 end
 
-#dynamically arranging position and level of the item in the  bay
-
-def bay
+#update the new id
+def create
   
- @id = params[:id]
- @poshash=Hash.new
- 
- position = Position.where(bay_id: @id)
- 
-     position.each do |posvalue|
-       
-      @poshash = @poshash.merge({posvalue.pos_id => {:type =>posvalue.properties1 , :item => posvalue.properties2}})
-       
-     end
-     
+   case params[:warehouse][:event]
+      when "update_position" 
+       newbay= Bay.where("bay_id = ?", params[:warehouse][:bay_id] ).first
+       newbay.customer_bay_id = params[:warehouse][:customer_bay_id] 
+       newbay.save
+   end    
+       render text: "ok"
+   #b= Posts.where("text = ?", params[:post][:text]).first
 end
 
+#dynamically arranging position and level of the item in the  bay
+
+  def bay
+  
+    @id = params[:id]
+    @levelhash=Hash.new
+    level = Level.where(bay_id: @id)
+    level.each do |levelvalue|
+      pos = Position.where("level_id = ? AND bay_id = ?", levelvalue.level_id, @id)
+      poshash=Hash.new
+      poshash= poshash.merge({:leveltype => levelvalue.properties1})
+      pos.each do |posvalue|
+        poshash = poshash.merge({posvalue.pos_id => {:postype => posvalue.properties1 , :item => posvalue.properties2}})
+        
+      end
+      @levelhash = @levelhash.merge(levelvalue.level_id  => poshash)
+    
+    end
+  
+  end
+  
+  
+  
 end
