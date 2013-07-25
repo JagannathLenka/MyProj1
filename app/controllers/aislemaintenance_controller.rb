c822a6b86ca475027ebe088f89fd06564d50cced
Mitali <Mitali@.(none)>
2013-07-25 13:14:32 -0700

    7/25/2013

--- app/controllers/aislemaintenance_controller.rb
+++ app/controllers/aislemaintenance_controller.rb
@@ -4,11 +4,11 @@ class AislemaintenanceController < ApplicationController
   def index
 
     @columns =  ['id','sm_aisle_id', 'cl_aisle_id','client_id', 'sm_zone_id','cl_zone_id','zone_id',
-                 'sm_warehouse_id' ,'cl_warehouse_id','description', 'no_of_bays_aisle', 'no_of_bays_aisle_hidden',
+                 'sm_warehouse_id' ,'cl_warehouse_id','description', 'no_of_bays_aisle', 'no_of_bays_aisle_hidden','no_of_levels_aisles','no_of_levels_aisles_hidden',
                   'attribute1', 'attribute2' , 'attribute3','attribute4',
                   'attribute5','attribute6','attribute7','attribute8']
     @aisles = Aisle.select(" id ,sm_aisle_id , cl_aisle_id ,client_id , sm_zone_id ,cl_zone_id , zone_id ,
-                 sm_warehouse_id , cl_warehouse_id,  description ,no_of_bays_aisle ,no_of_bays_aisle as no_of_bays_aisle_hidden,
+                 sm_warehouse_id , cl_warehouse_id,  description ,no_of_bays_aisle ,no_of_bays_aisle as no_of_bays_aisle_hidden,no_of_levels_aisles,no_of_levels_aisles as no_of_levels_aisles_hidden,
                   attribute1 , attribute2  , attribute3 ,attribute4 ,
                   attribute5, attribute6 , attribute7 , attribute8 ").where(:zone_id => params[:id]).paginate(
                    :page     => params[:page],
@@ -30,6 +30,7 @@ class AislemaintenanceController < ApplicationController
                                    :cl_aisle_id => params[:cl_aisle_id],
                                    :description    => params[:description],
                                    :no_of_bays_aisle => params[:no_of_bays_aisle],
+                                   :no_of_levels_aisles => params[:no_of_levels_aisles],
                                    :attribute1 => params[:attribute1],
                                    :attribute2 => params[:attribute2], 
                                    :attribute3 => params[:attribute3],
@@ -43,6 +44,7 @@ class AislemaintenanceController < ApplicationController
 
           create_bays
           
+          
   when "add"
         maximum_aisle_add = Aisle.maximum("sm_aisle_id").to_i + 1
         @aisles= Aisle.new(:sm_aisle_id => maximum_aisle_add, 
@@ -50,6 +52,7 @@ class AislemaintenanceController < ApplicationController
                                    :zone_id => params[:zone_id],
                                    :description  => params[:description],
                                    :no_of_bays_aisle => params[:no_of_bays_aisle],
+                                   :no_of_levels_aisles => param[:no_of_levels_aisles],
                                    :attribute1 => params[:attribute1],
                                    :attribute2 => params[:attribute2], 
                                    :attribute3 => params[:attribute3],
@@ -63,6 +66,7 @@ class AislemaintenanceController < ApplicationController
         
          @aisles.save 
          create_bays
+         
            
  end        
          
@@ -95,10 +99,12 @@ class AislemaintenanceController < ApplicationController
                              :cl_aisle_id         => @aisles.cl_aisle_id,
                              :cl_zone_id          => @aisles.cl_zone_id,
                              :cl_warehouse_id     => @aisles.cl_warehouse_id,
-                             :no_of_level_bay     => params[:no_of_level_bay],
-                             :attribute3          => side_of_aisle
+                             :no_of_level_bay     => @aisles.no_of_levels_aisles,
+                             :attribute3          => side_of_aisle,
+                             :no_of_level_bay     => @aisles.no_of_levels_aisles
                           )
              @bays.save
+             create_levels
          end
                  
      end
@@ -125,6 +131,34 @@ class AislemaintenanceController < ApplicationController
      end         
 
  
+ end
+ def create_levels
+   levelvalue = params[:no_of_levels_aisles].to_i
+   levelvalue_hidden = params[:no_of_levels_aisles_hidden].to_i
+   max_levels  = Level.where(:bay_id => @bays.id).maximum("sm_level_id")
+   if(levelvalue > levelvalue_hidden)
+      diff_levelvalue = levelvalue - levelvalue_hidden
+      (1..diff_levelvalue).each do |lev|
+        @levels = Level.new(:sm_level_id     => max_levels.to_i + lev,
+                             :sm_bay_id           => @bays.sm_bay_id,
+                             :sm_aisle_id         => @bays.sm_aisle_id,
+                             :sm_zone_id          => @bays.sm_zone_id,
+                             :sm_warehouse_id     => @bays.sm_warehouse_id,
+                             :bay_id              => @bays.id,
+                             :cl_level_id         => "",
+                             :cl_bay_id           => @bays.cl_bay_id,
+                             :cl_aisle_id         => @bays.cl_aisle_id,
+                             :cl_zone_id          => @bays.cl_zone_id,
+                             :cl_warehouse_id     => @bays.cl_warehouse_id,
+                             :no_of_pos_level     => ""
+                          )
+             @levels.save
+        
+      end
+        
+   
+     
+   end 
  end
 
 end
