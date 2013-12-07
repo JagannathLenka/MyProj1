3dc13d086aac197ae6284e2bfcecda030c07e97d
Mitali <Mitali@.(none)>
2013-12-06 18:05:45 -0700

    delayed job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131101021522) do
+ActiveRecord::Schema.define(:version => 20131207010259) do
 
   create_table "aisles", :force => true do |t|
     t.string   "client_id"
@@ -87,6 +87,22 @@ ActiveRecord::Schema.define(:version => 20131101021522) do
     t.datetime "updated_at",             :null => false
     t.string   "lastname_of_person"
   end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0, :null => false
+    t.integer  "attempts",   :default => 0, :null => false
+    t.text     "handler",                   :null => false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "itemmasters", :force => true do |t|
     t.string   "client_id"
