2411fcc1b8cd51831ece118f71022c2d70f14e27
unknown <jagannathp_lenka@BLRKEC97427L.ad.infosys.com>
2013-12-22 18:53:02 -0700

    item error

--- db/schema.rb
+++ db/schema.rb
@@ -338,10 +338,10 @@ ActiveRecord::Schema.define(:version => 20131222155906) do
     t.string   "attribute14"
     t.string   "attribute15"
     t.string   "attribute16"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
     t.string   "lock_code"
     t.string   "location_priority"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
   end
 
   create_table "positions", :force => true do |t|
@@ -382,11 +382,10 @@ ActiveRecord::Schema.define(:version => 20131222155906) do
     t.datetime "updated_at",      :null => false
   end
 
-  add_index "positions", ["level_id"], :name => "index_positions_on_level_id"
-
   create_table "slottingrecommendations", :force => true do |t|
     t.string   "client_id"
     t.string   "item_number"
+    t.string   "prffered_warehouse"
     t.string   "preffered_zone"
     t.string   "preffered_aisle"
     t.string   "preffered_bay"
@@ -406,7 +405,6 @@ ActiveRecord::Schema.define(:version => 20131222155906) do
     t.string   "attribute8"
     t.datetime "created_at",               :null => false
     t.datetime "updated_at",               :null => false
-    t.string   "preffered_warehouse"
     t.integer  "quantity_to_be_slotted"
   end
 
